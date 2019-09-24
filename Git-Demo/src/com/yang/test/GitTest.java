ad6426d35541d33458db3d38cc8e57cdd18c9c13
yixuan_glb <yangzy_glb@foxmail.com>
2019-09-24 21:21:06 +0800

    测试把本地工程添加到git

---
+++ Git-Demo/src/com/yang/test/GitTest.java
@@ -0,0 +1,13 @@
+package com.yang.test;
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!!!");
+        System.out.println("hello git!!!");
+        System.out.println("hello git!!!");
+        System.out.println("hello git!!!");
+        System.out.println("hello git!!!");
+        System.out.println("hello git!!!");
+        System.out.println("hello git!!!");
+    }
+}
