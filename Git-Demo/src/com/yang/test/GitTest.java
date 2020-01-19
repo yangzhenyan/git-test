1ec8d69a535c073fb330933b81c545c116717e50
test <49869822+yangzhenyan@users.noreply.github.com>
2020-01-19 19:34:32 +0800

    Update GitTest.java

--- Git-Demo/src/com/yang/test/GitTest.java
+++ Git-Demo/src/com/yang/test/GitTest.java
@@ -9,8 +9,6 @@ public class GitTest {
 		System.out.println("hello git!!!");
 		System.out.println("hello git!!!");
 		System.out.println("hello git!!!");
-
-		System.out.println("e3333nennenne");
 		System.out.println("nennenne");
 	}
 }
